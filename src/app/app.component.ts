b896ade7bae40732b25ea11bc9e763b819c9a541
flavio-h <flavio.pessini@outlook.com>
2019-11-25 14:44:04 -0300

    Finalizado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Todo } from './models/todo.model';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,76 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-curso-todo';
+  public pageTitle = 'Minhas Tarefas';
+
+  public mode = 'list';
+  public todoList: Todo[] = [];
+  public todoForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.buildForm();
+    this.load();
+  }
+
+  buildForm(): void {
+    this.todoForm = this.fb.group({
+      title: ['', Validators.compose([
+        Validators.required,
+        Validators.minLength(3),
+        Validators.maxLength(50),
+      ])],
+    });
+  }
+
+  clearForm(): void {
+    this.todoForm.reset();
+  }
+
+  changeMode(mode: string): void {
+    this.mode = mode;
+  }
+
+  add(): void {
+    const title = this.todoForm.controls.title.value;
+    const id = this.todoList.length + 1;
+    this.todoList.push(new Todo(id, title, false));
+    this.save();
+    this.clearForm();
+    this.mode = 'list';
+  }
+
+  toggleDone(todo: Todo): void {
+    todo.done = !todo.done;
+    this.save();
+  }
+
+  markAsDone(todo: Todo): void {
+    todo.done = true;
+    this.save();
+  }
+
+  markAsUndone(todo: Todo): void {
+    todo.done = false;
+    this.save();
+  }
+
+  remove(todo: Todo): void {
+    const index = this.todoList.indexOf(todo);
+    if (index !== -1) {
+      this.todoList.splice(index, 1);
+      this.save();
+    }
+  }
+
+  load(): void {
+    const data = JSON.parse(localStorage.getItem('TODOS'));
+    if (data) {
+      this.todoList = data;
+    }
+  }
+
+  save(): void {
+    const data = JSON.stringify(this.todoList);
+    localStorage.setItem('TODOS', data);
+  }
 }
